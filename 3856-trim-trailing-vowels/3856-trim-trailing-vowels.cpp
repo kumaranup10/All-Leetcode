f8401d402349070d6c109effd38a35bbec89544b
Kumar Anup <kumaranup9135072766@gmail.com>
2026-03-02 21:22:28 +0530

    Time: 0 ms (100%), Space: 8.7 MB (70%) - LeetHub

---
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string trimTrailingVowels(string s) {
+        int i=s.size()-1;
+
+        string temp="aeiou";
+
+        while(i >= 0 && temp.find(s[i]) != string::npos)
+        i--;
+
+        return s.substr(0,i+1);
+    }
+};
